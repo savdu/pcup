72cdadc09e904022d70b2fb24dcd167948207bc3
savdu <savannah.du@gmail.com>
2017-05-24 21:14:21 -0400

    first

---
+++ Part2.java
@@ -0,0 +1,7 @@
+public class Part2 {
+	public static void main(String[] args) {
+		String fileName = "dict.txt";
+        In in = new In(fileName);
+        String[] dictArray = in.readAllLines(); // all dictionary words are now contained in dictArray
+	}
+}
